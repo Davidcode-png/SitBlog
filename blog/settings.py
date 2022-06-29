6b27678946855aa97bb2c56de455d758947b5873
DavidsonCo <opeoluwa313@gmail.com>
2022-06-29 17:33:15 +0100

    Heroku Stuff

--- blog/settings.py
+++ blog/settings.py
@@ -27,7 +27,7 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = os.environ['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
