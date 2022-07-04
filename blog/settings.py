ca6233a1e53207e5e95968859be64b92c72d76b0
DavidsonCo <opeoluwa313@gmail.com>
2022-07-04 18:16:00 +0100

    AWS S3 Bucket

--- blog/settings.py
+++ blog/settings.py
@@ -209,6 +209,6 @@ STATIC_URL = 'https://'+AWS_STORAGE_BUCKET_NAME+ '.s3.amazonaws.com/'
 # AWS_S3_FILE_OVERWRITE = False
 # AWS_DEFAULT_ACL = None
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3BotoStorage'
 
 ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
